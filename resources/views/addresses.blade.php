84f9ae2acc5a459dbcb40bf406f8e00ce789ef88
Saif Hatem <saifxhatem@gmail.com>
2020-12-21 16:35:34 +0200

    Added insert_addresses feature

--- resources/views/addresses.blade.php
+++ resources/views/addresses.blade.php
@@ -21,11 +21,11 @@
        @csrf
         <div class="form-group">
           <label for="exampleInputEmail1">Address</label>
-          <input type="text" id="title" name="address" class="form-control">
+          <input type="text" id="title" name="address" class="form-control" required="">
         </div>
         <div class="form-group">
           <label for="exampleInputEmail1">User_ID</label>
-          <input type="text" id="title" name="user_id" class="form-control">
+          <input type="number" id="title" name="user_id" class="form-control" required="">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
